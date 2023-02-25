fd7bc28e3ba0397493e3612faf008813ebe8c009
kouji777 <118167498+kouji777@users.noreply.github.com>
2023-02-26 02:50:16 +0900

    新規登録

--- db/migrate/20230218184351_devise_create_users.rb
+++ db/migrate/20230218184351_devise_create_users.rb
@@ -11,11 +11,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name_kana, null: false, default: ""
       t.string :family_name, null: false, default: ""
       t.string :family_name_kana, null: false, default: ""
-      t.date :, null: false, default: ""
-
-
-
-
 
       ## Recoverable
       t.string   :reset_password_token
