99f13ced007ab728e77d4018426141b2f5fccca9
kouji777 <118167498+kouji777@users.noreply.github.com>
2023-03-02 14:27:25 +0900

    Rubocop修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,6 @@ class User < ApplicationRecord
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
-
     validates :nickname, presence: true
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
