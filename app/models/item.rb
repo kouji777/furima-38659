347f70618070b0e3b261916d29c93215f8be21e7
kouji777 <118167498+kouji777@users.noreply.github.com>
2023-03-13 19:33:39 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -28,7 +28,7 @@ class Item < ApplicationRecord
     validates :shipment_id
     # 300円以上かつ9,999,999円以下で、半角数字かつ整数値のみでないと入力不可
     validates :price, numericality: {only_integer:true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-    validates :price, format: { with: /\A[0-9]+\z/ } 
+  
   end
 
   # ジャンルの選択が「--」の時は保存不可
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe Item, type: :model do
         @item.price = 300
         expect(@item).to be_valid
       end
-      
+
     end
 
     context '出品ができないとき' do
@@ -121,6 +121,11 @@ RSpec.describe Item, type: :model do
         @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end 
+      it '価格が半角数値以外だと出品できない' do
+        @item.price = 'ああああああ'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
     end
   end
