3de6342650d0cddd1acc2a503db0e74e65634eaf
kouji777 <118167498+kouji777@users.noreply.github.com>
2023-03-22 13:42:12 +0900

    model,controller,route作成

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,31 +1,39 @@
-# class PurchaseRecordsController < ApplicationController
-#   before_action :authenticate_user!
-#   before_action :non_purchased_item, only: [:index, :create]
+ class PurchaseRecordsController < ApplicationController
+   before_action :authenticate_user!
+   before_action :non_purchased_item, only: [:index, :create]
 
-#   def index
-#     @purchase_record_form = PurchaseRecordForm.new
-#   end
+   def index
+     @purchase_record_form = PurchaseRecordForm.new
+   end
 
-#   def create
-#     @purchase_record_form = PurchaseRecordForm.new(purchase_record_params)
-#     if @purchase_record_form.valid?
-#       pay_item
-#       @purchase_record_form.save
-#       redirect_to root_path
-#     else
-#       render :index
-#     end
-#   end
+   def create
+     @purchase_record_form = PurchaseRecordForm.new(purchase_record_params)
+     if @purchase_record_form.valid?
+       pay_item
+       @purchase_record_form.save
+       redirect_to root_path
+     else
+       render :index
+     end
+   end
 
-#   private
+   private
 
-#   def purchase_record_params
-#     params.require(:purchase_record_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
-#   end
+   def purchase_record_params
+     params.require(:purchase_record_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+   end
 
-#   def non_purchased_item
-#     # itemがあっての、order_form（入れ子構造）。他のコントローラーで生成されたitemを使うにはcreateアクションに定義する。
-#     @item = Item.find(params[:item_id])
-#     redirect_to root_path if current_user.id == @item.user_id || @item.purchase_records.present?
-#   end
-# end
+   def pay_item
+     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+     Payjp::Charge.create(
+      amount: @item.price,       
+      card: purcase_record_params[:token], 
+      currency: 'jpy'             
+    )
+   end
+
+   def non_purchased_item
+     @item = Item.find(params[:item_id])
+     redirect_to root_path if current_user.id == @item.user_id || @item.purchase_records.present?
+   end
+ end
